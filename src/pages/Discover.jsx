9370657d418b43507331ef3e53cfccefb31e1aa1
Mike <michalkaso@yahoo.com>
2022-10-25 17:32:58 +0100

    side menu component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,17 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
-
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
   if (isFetching) return <Loader title="Loading songs..." />;
   if (error) return <Error />;
 
- 
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between
@@ -19,27 +22,30 @@ const Discover = () => {
       >
         <h2 className="font-bold text-3xl text-white text-left"> Discover {genreTitle}</h2>
         <select
-            onChange={() => {}}
-            value=""
-            className="bg-black text-gray-300 p-3 
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3
                     text-sm rounded-lg outiline-none sm:mt-0 mt-5"
         >
-            {genres.map((genre) => 
-                <option key={genre.value} value={genre.value}>
-                    {genre.title}
-                </option> 
-            )}
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
         </select>
       </div>
 
       <div className="flex flex-warp sm:justify-start justify-center gap-8">
-            {data?.map((song, i) => (
-                <SongCard 
-                    key={song.key}
-                    song={song}
-                    i = {i}
-                />
-            ))}
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
       </div>
     </div>
   );
